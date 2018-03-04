b203f6a5d8fc20cf751ed772848f175c46892a35
Orumiya <kasza.eszter.katalin@gmail.com>
2018-03-04 11:50:11 +0100

    Customer class created

--- HappyTourManager/HappyTourManager/Data/Customer.cs
+++ HappyTourManager/HappyTourManager/Data/Customer.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HappyTourManager.Data
+﻿namespace HappyTourManager.Data
 {
-    private class Customer : ICrudFunctions
-    {
+    using HappyTourManager.Interface;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
+    public class Customer : ICrudFunctions
+    {
+        public void CreateEntry(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEntry(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditEntry(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<object> ListAllEntry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object SearchEntry(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
