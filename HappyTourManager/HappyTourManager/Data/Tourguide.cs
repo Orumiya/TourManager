b435868f91919a00f52702a14be72d6f57255f41
Orumiya <kasza.eszter.katalin@gmail.com>
2018-03-04 22:06:44 +0100

    Tourguide class added

---
+++ HappyTourManager/HappyTourManager/Data/Tourguide.cs
@@ -0,0 +1,20 @@
+﻿namespace HappyTourManager.Data
+{
+    using HappyTourManager.Interface;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Tourguide class is to handle the data of the tourguides of the travel agency.
+    /// Tourguide class has inherited properties from the base class "Person"
+    /// </summary>
+    public class Tourguide : Person
+    {
+        public int TaxID { get; set; }
+
+        public int DailyAllowance { get; set; }
+    }
+}
