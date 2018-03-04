29adeed575a5fd77246c88e9b67870a9afed259c
Orumiya <kasza.eszter.katalin@gmail.com>
2018-03-04 10:48:05 +0100

    Logger class added

---
+++ HappyTourManager/HappyTourManager/Helper/Logger.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="Logger.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace HappyTourManager.Helper
+{
+    using System;
+
+    /// <summary>
+    /// The class is to help debugging, providing useful log messages
+    /// Example:
+    /// Logger.AddLogMethod(Console.WriteLine);
+    /// Logger.AddLogMethod(m =>
+    ///       { using (StreamWriter sw =
+    ///            new StreamWriter("log.txt", true))
+    ///            {
+    ///              sw.WriteLine(m);
+    ///            }
+    ///  });
+    /// Logger.Log("az adatbáziskapcsolat felépült");
+    /// </summary>
+    public static class Logger
+    {
+        private static Action<string> log;
+
+        public static void AddLogMethod(Action<string> logMethod)
+        {
+            log += logMethod;
+        }
+
+        public static void Log(string message)
+        {
+            log("[" + DateTime.Now.ToString() + "]" + message);
+        }
+    }
+}
--- HappyTourManager/HappyTourManager/MainWindow.xaml.cs
+++ HappyTourManager/HappyTourManager/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ namespace HappyTourManager
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -18,6 +19,7 @@ namespace HappyTourManager
     using System.Windows.Media.Imaging;
     using System.Windows.Navigation;
     using System.Windows.Shapes;
+    using Helper;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
