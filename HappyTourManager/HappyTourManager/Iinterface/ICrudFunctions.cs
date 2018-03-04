f40016d5cf1b9049285daa00b510a5bee327739e
Orumiya <kasza.eszter.katalin@gmail.com>
2018-03-04 11:37:06 +0100

    ICrudFunctions interface added

---
+++ HappyTourManager/HappyTourManager/Iinterface/ICrudFunctions.cs
@@ -0,0 +1,30 @@
+﻿namespace HappyTourManager.Iinterface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// CRUD Interface:
+    /// The software must allow the user to
+    /// - Create or add new entries
+    /// - Read, retrieve, search, or view existing entries
+    /// - Update or edit existing entries
+    /// - Delete/deactivate/remove existing entries
+    /// These functionalities is provided by the ICrudFunctions interface.
+    /// </summary>
+    public interface ICrudFunctions
+    {
+        void CreateEntry(object obj);
+
+        object SearchEntry(object obj);
+
+        List<object> ListAllEntry();
+
+        void EditEntry(object obj);
+
+        void DeleteEntry(object obj);
+    }
+}
