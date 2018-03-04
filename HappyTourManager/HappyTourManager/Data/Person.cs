be3e00765ddafb989df04115447f644b4c8801e7
Orumiya <kasza.eszter.katalin@gmail.com>
2018-03-04 11:51:44 +0100

    Person class created

---
+++ HappyTourManager/HappyTourManager/Data/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace HappyTourManager.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Person
+    {
+         
+    }
+}
