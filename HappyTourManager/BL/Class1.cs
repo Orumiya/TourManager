dc362eb41dd45259886acb2dddb80ecba73effff
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-15 23:10:49 +0100

    adding references to class libraries

--- HappyTourManager/BL/Class1.cs
+++ HappyTourManager/BL/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DATA;
 
 namespace BL
 {
     public class Class1
     {
+        
     }
 }
