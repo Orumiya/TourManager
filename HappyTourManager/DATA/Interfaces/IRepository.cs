a4fcca150a8be2d019f6a76c94760b22394eca69
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-24 15:38:07 +0100

    IRepository interface added

---
+++ HappyTourManager/DATA/Interfaces/IRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace DATA.Interfaces
+{
+    using System.Linq;
+
+    public interface IRepository<T>
+    {
+        /// <summary>
+        /// returns all data objects
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<T> GetAll();
+
+        /// <summary>
+        /// Save data (generic method)
+        /// </summary>
+        /// <param name="dataobject"></param>
+        void Save(T dataobject);
+
+        /// <summary>
+        /// Delete dataobject (generic method)
+        /// </summary>
+        /// <param name="dataobject"></param>
+        void Delete(T dataobject);
+
+        /// <summary>
+        /// throws exception, if same object exists
+        /// </summary>
+        /// <param name="dataobject"></param>
+        void ThrowIfExists(T dataobject);
+    }
+}
