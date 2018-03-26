0b1826eb3bcaaca2b4f8257c572fb05bfbedcea9
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-26 22:31:30 +0200

    TourguideRepository added

---
+++ HappyTourManager/DATA/Repositoriees/TourguideRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DATA.Interfaces;
+
+namespace DATA.Repositoriees
+{
+    class TourguideRepository : IRepository<Tourguide>
+    {
+        /// <summary>
+        /// field to Database
+        /// </summary>
+        private HappyTourDatabaseEntities entities;
+
+        /// <summary>
+        /// creates the repository
+        /// </summary>
+        /// <param name="entities"></param>
+        public TourguideRepository(HappyTourDatabaseEntities entities)
+        {
+            this.entities = entities;
+        }
+    }
+}
