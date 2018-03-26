37fa9100d4b492940056c2b22ec78275689bf886
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-26 22:29:02 +0200

    TourRepository added

---
+++ HappyTourManager/DATA/Repositoriees/TourRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace DATA.Repositoriees
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DATA.Interfaces;
+
+    class TourRepository : IRepository<Tour>
+    {
+        /// <summary>
+        /// field to Database
+        /// </summary>
+        private HappyTourDatabaseEntities entities;
+
+        /// <summary>
+        /// creates the repository
+        /// </summary>
+        /// <param name="entities"></param>
+        public TourRepository(HappyTourDatabaseEntities entities)
+        {
+            this.entities = entities;
+        }
+    }
+}
