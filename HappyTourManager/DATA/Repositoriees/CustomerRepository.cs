ae9d215f050ec4d624eabfddd17cd802cb27fc9d
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-26 22:24:18 +0200

    CustomerRepository added

---
+++ HappyTourManager/DATA/Repositoriees/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace DATA.Repositoriees
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DATA.Interfaces;
+
+    public enum CustomerTerms
+    {
+
+    }
+    public class CustomerRepository : IRepository<Customer>
+    {
+        /// <summary>
+        /// field to Database
+        /// </summary>
+        private HappyTourDatabaseEntities entities;
+
+        /// <summary>
+        /// creates the repository
+        /// </summary>
+        /// <param name="entities"></param>
+        public CustomerRepository(HappyTourDatabaseEntities entities)
+        {
+            this.entities = new HappyTourDatabaseEntities();
+        }
+
+
+
+    }
+}
