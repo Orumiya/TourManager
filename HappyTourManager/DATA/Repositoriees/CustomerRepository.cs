606fe26152fe24e475c019d251e32ed18519a9d5
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-27 21:47:19 +0200

    IRepository IF added to CustomerRepository

--- HappyTourManager/DATA/Repositoriees/CustomerRepository.cs
+++ HappyTourManager/DATA/Repositoriees/CustomerRepository.cs
@@ -1,9 +1,7 @@
 ﻿namespace DATA.Repositoriees
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using DATA.Interfaces;
 
     public enum CustomerTerms
@@ -26,7 +24,34 @@
             this.entities = new HappyTourDatabaseEntities();
         }
 
+        public void Create(Customer dataobject)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(Customer dataobject)
+        {
+            throw new NotImplementedException();
+        }
 
+        public IQueryable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Customer> Search(object searchterm, object searchvalue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowIfExists(Customer dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
