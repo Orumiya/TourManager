ecfb1622facf7b9d549d77821480101c50a14662
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-27 21:58:45 +0200

    LanguageRepository added

---
+++ HappyTourManager/DATA/Repositoriees/LanguageRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace DATA.Repositoriees
+{
+    using DATA.Interfaces;
+    using System;
+    using System.Linq;
+
+    class LanguageRepository : IRepository<Language>
+    {
+        /// <summary>
+        /// field to Database
+        /// </summary>
+        private HappyTourDatabaseEntities entities;
+
+        /// <summary>
+        /// creates the repository
+        /// </summary>
+        /// <param name="entities"></param>
+        public LanguageRepository(HappyTourDatabaseEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        public void Create(Language dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Language dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Language> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Language> Search(object searchterm, object searchvalue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowIfExists(Language dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
