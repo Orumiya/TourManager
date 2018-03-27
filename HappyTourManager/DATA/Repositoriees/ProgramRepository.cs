bf741cb1c17e0052b5ee3b1fbd07e3d97f377834
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-27 21:54:02 +0200

    ProgramRepository added

---
+++ HappyTourManager/DATA/Repositoriees/ProgramRepository.cs
@@ -0,0 +1,52 @@
+﻿namespace DATA.Repositoriees
+{
+    using DATA.Interfaces;
+    using System;
+    using System.Linq;
+
+    class ProgramRepository : IRepository<Program>
+    {
+        /// <summary>
+        /// field to Database
+        /// </summary>
+        private HappyTourDatabaseEntities entities;
+
+        /// <summary>
+        /// creates the repository
+        /// </summary>
+        /// <param name="entities"></param>
+        public ProgramRepository(HappyTourDatabaseEntities entities)
+        {
+            this.entities = entities;
+        }
+        public void Create(Program dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Program dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Program> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Program> Search(object searchterm, object searchvalue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowIfExists(Program dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
