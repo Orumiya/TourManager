db58150bc6a2cf0b2601b344d3346164adcba835
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-03-27 21:56:58 +0200

    OnholidayRepository added

---
+++ HappyTourManager/DATA/Repositoriees/OnholidayRepository.cs
@@ -0,0 +1,52 @@
+﻿namespace DATA.Repositoriees
+{
+    using DATA.Interfaces;
+    using System;
+    using System.Linq;
+
+    class OnholidayRepository : IRepository<OnHoliday>
+    {
+        /// <summary>
+        /// field to Database
+        /// </summary>
+        private HappyTourDatabaseEntities entities;
+
+        /// <summary>
+        /// creates the repository
+        /// </summary>
+        /// <param name="entities"></param>
+        public OnholidayRepository(HappyTourDatabaseEntities entities)
+        {
+           this.entities = entities;
+        }
+        public void Create(OnHoliday dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(OnHoliday dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<OnHoliday> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<OnHoliday> Search(object searchterm, object searchvalue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowIfExists(OnHoliday dataobject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
