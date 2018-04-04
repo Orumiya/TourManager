a7733e42604b78a85d53e96933407e367b829ca3
Barbara Böszöri <eboszori@gmail.com>
2018-04-04 11:02:16 +0200

    ValueConverter abstract class added

---
+++ HappyTourManager/HappyTourManager/Converters/ValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace HappyTourManager
+{
+    public abstract class ValueConverter<T> : MarkupExtension, IValueConverter where T: class, new()
+    {
+        private static T converter = null;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (converter != null)
+            {
+                return converter;
+            }
+            else
+            {
+                converter = new T();
+                return converter;
+            }
+        }
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+    }
+}
