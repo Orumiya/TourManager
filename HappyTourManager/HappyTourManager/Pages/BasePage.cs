8e7b407c0ecf10e0cc85895c8888536d669f8016
Barbara Böszöri <eboszori@gmail.com>
2018-04-06 15:37:19 +0200

    BasePage class added

---
+++ HappyTourManager/HappyTourManager/Pages/BasePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using HappyTourManager.ViewModels;
+
+namespace HappyTourManager.Pages
+{
+    public class BasePage<T> : Page where T : Bindable, new()
+    {
+        private T viewModel;
+
+        public T ViewNodel
+        {
+            get
+            { return viewModel; }
+            set
+            {
+                if (viewModel == value)
+                {
+                    return;
+                }
+
+                viewModel = value;
+                this.DataContext = viewModel;
+            }
+        }
+
+        public BasePage()
+        {
+            this.ViewNodel= new T();
+        }
+
+    }
+}
