e37fbab7095f1b4adb2fcbd82babb2759cc0aa75
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-10 09:20:58 +0200

    ISearcheable IF added to BL

---
+++ HappyTourManager/BL/Interfaces/ISearcheable.cs
@@ -0,0 +1,15 @@
+﻿namespace BL.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface ISearcheable<T>
+    {
+        /// <summary>
+        /// searches for a dataobject with specified searchterm and value
+        /// </summary>
+        /// <param name="searchterm"></param>
+        /// <param name="searchvalue"></param>
+        /// <returns></returns>
+        IList<T> Search(object searchterm, object searchvalue);
+    }
+}
