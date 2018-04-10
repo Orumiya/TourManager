dabbf0d89c2113bc05d2de74955c30ed3a63cc7b
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-10 14:05:02 +0200

    comments added

--- HappyTourManager/DATA/Interfaces/IRepository.cs
+++ HappyTourManager/DATA/Interfaces/IRepository.cs
@@ -7,13 +7,13 @@
         /// <summary>
         /// returns all data objects
         /// </summary>
-        /// <returns></returns>
+        /// <returns>output</returns>
         IQueryable<T> GetAll();
 
         /// <summary>
         /// create new dataobject
         /// </summary>
-        /// <param name="dataobject"></param>
+        /// <param name="dataobject">input param</param>
         void Create(T dataobject);
 
         /// <summary>
@@ -24,14 +24,13 @@
         /// <summary>
         /// Delete dataobject (generic method)
         /// </summary>
-        /// <param name="dataobject"></param>
+        /// <param name="dataobject">input param</param>
         void Delete(T dataobject);
 
         /// <summary>
         /// throws exception, if same object exists
         /// </summary>
-        /// <param name="dataobject"></param>
+        /// <param name="dataobject">input param</param>
         void ThrowIfExists(T dataobject);
-
     }
 }
