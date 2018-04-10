300d1dfecd08d8115073d7476ae99fbebb3c8bda
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-10 14:24:04 +0200

    TourguideBL created

---
+++ HappyTourManager/BL/TourguideBL.cs
@@ -0,0 +1,40 @@
+﻿using BL.Interfaces;
+using DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class TourguideBL : ISearcheable<Tourguide>, ITourguideList
+    {
+        /// <inheritdoc />
+        public event EventHandler TourguideListChanged;
+
+        /// <inheritdoc />
+        public void Delete(Tourguide guide)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void Save(Tourguide guide)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IList<Tourguide> Search(object searchterm, object searchvalue)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void ThrowIfExists(Tourguide guide)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
