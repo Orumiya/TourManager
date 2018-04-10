c65bf8b23a2e502cdb0da4a97b59704f081aa759
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-10 23:48:55 +0200

    TourguideBLTest created

--- HappyTourManager/TEST/TourguideBLTest.cs
+++ HappyTourManager/TEST/TourguideBLTest.cs
@@ -3,12 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BL;
+using DATA;
+using DATA.Repositoriees;
 using NUnit.Framework;
+
 
 namespace TEST
 {
     //[TestFixture]
     public class TourguideBLTest
     {
+        [Test]
+        public void WhenCreatingTourguideBL_ThenTourguideBLIsNotNull()
+        {
+            // ARRANGE - ACT
+            HappyTourDatabaseEntities entities = new HappyTourDatabaseEntities();
+            TourguideRepository tourguideRepository = new TourguideRepository(entities);
+            LanguageRepository languageRepository = new LanguageRepository(entities);
+            OnholidayRepository onHolidayRepository = new OnholidayRepository(entities);
+            TourguideBL bl = new TourguideBL(tourguideRepository, languageRepository, onHolidayRepository);
+
+            // ASSERT
+            Assert.That(bl, Is.Not.Null);
+        }
+
+        [Test]
+        public void WhenCreatingNewTourguide_ThenTourguideIsSaved()
+        {
+            // ARRANGE 
+            //TourguideBL bl = new TourguideBL(tourguideRepository, languageRepository, onHolidayRepository);
+            ///ACT
+        }
     }
 }
