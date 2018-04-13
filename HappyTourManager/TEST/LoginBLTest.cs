ebb4728ddb1a4d5158bd36b91f3e676d81239d4f
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-13 12:01:43 +0200

    LoginBLTest created

---
+++ HappyTourManager/TEST/LoginBLTest.cs
@@ -0,0 +1,37 @@
+﻿using DATA;
+using DATA.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEST.Fakes;
+
+namespace TEST
+{
+    [TestFixture]
+    class LoginBLTest
+    {
+        private FakeRepository<User> userRepository;
+        private User[] users;
+
+        public LoginBLTest()
+        {
+            CreateUserTestArray();
+            this.userRepository = new FakeRepository<User>(users);
+        }
+
+        private void CreateUserTestArray()
+        {
+            users = new[]
+            {
+                new User
+                {
+                    Username = "user1",
+                    Password = "0A041B9462CAA4A31BAC3567E0B6E6FD9100787DB2AB433D96F6D178CABFCE90"
+                }
+            };
+        }
+    }
+}
