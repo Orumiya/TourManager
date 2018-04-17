85690f068e20fd4f5e9f34cf9d57a52772efac21
Barbara Böszöri <eboszori@gmail.com>
2018-04-17 13:59:41 +0200

    CreateContainer method added

--- HappyTourManager/HappyTourManager/AutoGUIBuilder.cs
+++ HappyTourManager/HappyTourManager/AutoGUIBuilder.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace HappyTourManager
@@ -19,6 +21,24 @@ namespace HappyTourManager
         private void BuildGUI(StackPanel stackPanel, object content)
         {
             Type t = content.GetType();
+            foreach (var propInfo in t.GetProperties())
+            {
+                var editor = CreateEditor(propInfo, content);
+                if (editor != null)
+                {
+                    var container = CreateContainer(propInfo, editor);
+                    stackPanel.Children.Add(container);
+                }
+
+
+            }
         }
+
+        private Panel CreateContainer(PropertyInfo propInfo, object editor)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
