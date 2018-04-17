f1a5dccf04467974202802b94fef08976f777268
Barbara Böszöri <eboszori@gmail.com>
2018-04-17 13:17:30 +0200

    AutoGUIBuilder class added

---
+++ HappyTourManager/HappyTourManager/AutoGUIBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HappyTourManager
+{
+    class AutoGUIBuilder
+    {
+        public AutoGUIBuilder(Panel parent, object content)
+        {
+            var stackPanel = new StackPanel();
+            parent.Children.Add(stackPanel);
+        }
+    }
+}
