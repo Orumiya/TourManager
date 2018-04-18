accaf7668e7f85a6280dabd0ceb9a7557ab9c020
Barbara Böszöri <eboszori@gmail.com>
2018-04-18 15:34:47 +0200

    ViewModels namespace changed

--- HappyTourManager/HappyTourManager/ViewModels/LoginViewModel.cs
+++ HappyTourManager/HappyTourManager/ViewModels/LoginViewModel.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace HappyTourManager.ViewModels
+namespace HappyTourManager
 {
     class LoginViewModel : Bindable
     {
