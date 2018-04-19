820d376b3c0f88f75c4db4a60df14dcd764f20c3
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-19 22:04:12 +0200

    CustomerBLTest added

---
+++ HappyTourManager/TEST/CustomerBLTest.cs
@@ -0,0 +1,71 @@
+﻿using BL;
+using DATA;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEST.Fakes;
+
+namespace TEST
+{
+    [TestFixture]
+    class CustomerBLTest
+    {
+        private Customer[] customers;
+        private CustomerBL bl;
+        private FakeRepository<Customer> customerRepository;
+
+        public CustomerBLTest()
+        {
+            CreateTestdataArrays();
+            this.customerRepository = new FakeRepository<Customer>(customers);
+        }
+
+        private void CreateTestdataArrays()
+        {
+            customers = new[]
+            {
+                new Customer()
+                {
+                LoyaltyCard = '1',
+                Person = new Person()
+                    {
+                        FirstName = "Marcus",
+                        LastName = "Aurelius",
+                        BirthDate = new DateTime(1978, 10, 2),
+                        IDNumber = 231234567,
+                        IDType = "identity card",
+                        AddressCity = "Miskolc",
+                        AddressCountry = "Hungary",
+                        AddressFree = "Kossuth u 34",
+                        AddressZip = "3200",
+                        Phone = 063012312312,
+                        ValidTo = new DateTime(2025, 02, 02)
+                    }
+                },
+                new Customer()
+                {
+                    LoyaltyCard = '0',
+                    Person = new Person()
+                    {
+                        FirstName = "Vanessa",
+                        LastName = "Williams",
+                        BirthDate = new DateTime(1978, 10, 2),
+                        IDNumber = 231234567,
+                        IDType = "identity card",
+                        AddressCity = "Budapest",
+                        AddressCountry = "Hungary",
+                        AddressFree = "Kossuth u 34",
+                        AddressZip = "3200",
+                        Phone = 063012312312,
+                        ValidTo = new DateTime(2020, 02, 02)
+                    }
+                }
+            };
+        }
+
+
+    }
+}
