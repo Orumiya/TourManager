719d96ec1576ec675c35d3b68cf4c4ddf45078cc
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-22 14:31:24 +0200

    OrderBL added

---
+++ HappyTourManager/BL/OrderBL.cs
@@ -0,0 +1,61 @@
+﻿using BL.Interfaces;
+using DATA;
+using DATA.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class OrderBL : ISearcheable<Order>, IOrderList
+    {
+        private readonly IRepository<Order> orderRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderBL"/> class.
+        /// creates the OrderBL
+        /// </summary>
+        /// <param name="orderRepository">input repository</param>
+        public OrderBL(IRepository<Order> orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+
+        /// <inheritdoc />
+        public event EventHandler OrderListChanged;
+
+        /// <inheritdoc />
+        public void Delete(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void Save(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IList<Order> Search(object searchterm, object searchvalue)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void ThrowIfExists(Order order)
+        {
+            this.orderRepository.ThrowIfExists(order);
+        }
+
+        /// <summary>
+        /// notifies the outside about any collection change manually
+        /// </summary>
+        private void OnOrderListChanged()
+        {
+            this.OrderListChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
