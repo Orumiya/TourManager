92cc40ed32c140bf4e2fc57aa84bb0e87ce509a6
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-28 13:15:39 +0200

    IUpdateable IF added to BL

---
+++ HappyTourManager/BL/Interfaces/IUpdateable.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IUpdateable.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BL.Interfaces
+{
+    public interface IUpdateable
+    {
+        /// <summary>
+        /// updates an entry
+        /// </summary>
+        void Update(); 
+    }
+}
