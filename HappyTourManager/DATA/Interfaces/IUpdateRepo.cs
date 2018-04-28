61ee1a638e7eff3e4369560561e35613221bb1a0
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-28 13:11:49 +0200

    IUpdateRepo IF created

---
+++ HappyTourManager/DATA/Interfaces/IUpdateRepo.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IUpdateRepo.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace DATA.Interfaces
+{
+    public interface IUpdateRepo
+    {
+        /// <summary>
+        /// updates a repository
+        /// </summary>
+        void Update();
+    }
+}
