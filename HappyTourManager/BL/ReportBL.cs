a1bf51e84455136f66b633d8741053d52e950e7f
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-29 13:45:33 +0200

    ISearcheable IF added to reportBL

--- HappyTourManager/BL/ReportBL.cs
+++ HappyTourManager/BL/ReportBL.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="ReportBL.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace BL
 {
-    class ReportBL
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using BL.Interfaces;
+    using DATA;
+    using DATA.Interfaces;
+
+    public class ReportBL : ISearcheable<Report>
     {
+        public IList<Report> Search(object searchterm, object searchvalue)
+        {
+            throw new NotImplementedException();
+        }
     }
+}
 }
