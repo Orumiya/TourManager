b0d8f75b644b3e139bb28e654d5377e151626f5e
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-29 17:46:42 +0200

    connecting MainWindow to DB

--- HappyTourManager/HappyTourManager/MainWindow.xaml.cs
+++ HappyTourManager/HappyTourManager/MainWindow.xaml.cs
@@ -24,12 +24,26 @@ namespace HappyTourManager
     using DATA;
     using DATA.Repositories;
     using DATA.Interfaces;
+    using DATA.Repositoriees;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HappyTourDatabaseEntities entities;
+        private IRepository<Report> reportRepository;
+        private IRepository<Order> orderRepository;
+        private IRepository<Customer> customerRepository;
+        private IRepository<Tour> tourRepository;
+        private IRepository<Tourguide> tourguideRepository;
+        private IRepository<Language> languageRepository;
+        private IRepository<OnHoliday> onHolidayRepository;
+        private IRepository<Program> programRepository;
+        private IRepository<Place> placeRepository;
+        private IRepository<PLTCON> pltconRepository;
+        private IRepository<PRTCON> prtconRepository;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// a főmenü page-t indítja
@@ -41,6 +55,22 @@ namespace HappyTourManager
             //this.mainFrame.Content = page1;
 
             this.DataContext = new WindowViewModel(this);
+        }
+
+        private void AppWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.entities = new HappyTourDatabaseEntities();
+            this.reportRepository = new ReportRepository(entities);
+            this.orderRepository = new OrderRepository(entities);
+            this.customerRepository = new CustomerRepository(entities);
+            this.tourguideRepository = new TourguideRepository(entities);
+            this.tourRepository = new TourRepository(entities);
+            this.languageRepository = new LanguageRepository(entities);
+            this.onHolidayRepository = new OnholidayRepository(entities);
+            this.programRepository = new ProgramRepository(entities);
+            this.placeRepository = new PlaceRepository(entities);
+            this.pltconRepository = new PLTCONRepository(entities);
+            this.prtconRepository = new PRTCONRepository(entities);
         }
     }
 }
