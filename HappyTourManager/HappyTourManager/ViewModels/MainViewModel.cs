9504eed62a94377b0e576f2f7ca4fdb011ee1283
Barbara Böszöri <eboszori@gmail.com>
2018-04-29 19:54:35 +0200

    MainViewModel added

---
+++ HappyTourManager/HappyTourManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BL;
+using DATA;
+using DATA.Repositoriees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTourManager
+{
+    class MainViewModel
+    {
+        //private CustomerRepository custRepository;
+        //private CustomerBL custBL;
+        HappyTourDatabaseEntities entities;
+        private string selectedPage;
+
+        public MainViewModel(HappyTourDatabaseEntities entities)
+        {
+            this.entities = entities;
+            
+        }
+
+    }
+}
