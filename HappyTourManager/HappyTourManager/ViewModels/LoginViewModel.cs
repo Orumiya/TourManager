59a9b6c457a06eda23bac0f5d73a638299c7f4e9
Barbara Böszöri <eboszori@gmail.com>
2018-04-29 19:54:17 +0200

    LoginViewModel modifed

--- HappyTourManager/HappyTourManager/ViewModels/LoginViewModel.cs
+++ HappyTourManager/HappyTourManager/ViewModels/LoginViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BL;
+using DATA;
+using DATA.Interfaces;
+using DATA.Repositoriees;
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
@@ -11,13 +15,47 @@ namespace HappyTourManager
 {
     class LoginViewModel : Bindable
     {
-        public string Username { get; set; }
+        private UserRepository userRepository;
+        private string userName;
+        private string password;
+        public LoginBL loginBL;
 
-        public SecureString Password { get; set; }
+        public string Username {
+            get { return userName; }
+            set
+            {
+                userName = value;
+                OnPropertyChanged(nameof(Username));
+            }
 
-        
+        }
+
+        public string Password {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+
+        }
+
+        public LoginViewModel(HappyTourDatabaseEntities entities)
+        {
+            userRepository = new UserRepository(entities);
+            loginBL = new LoginBL(userRepository);
+        }
 
 
+        public bool SignIn()
+        {
+            return loginBL.Login(Username, Password);
+        }
+
+        public bool SignUp()
+        {
+            return loginBL.RegisterUser(Username, Password);
+        }
 
     }
 }
