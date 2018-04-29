41aba857ed15e714b363a3ada0e02fb9e0b22501
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-04-29 13:46:43 +0200

    ctor implemented to reportBL

--- HappyTourManager/BL/ReportBL.cs
+++ HappyTourManager/BL/ReportBL.cs
@@ -13,7 +13,43 @@ namespace BL
 
     public class ReportBL : ISearcheable<Report>, IReportList
     {
-        
+        private readonly IRepository<Report> reportRepository;
+        private readonly IRepository<Order> orderRepository;
+        private readonly IRepository<Customer> customerRepository;
+        private readonly IRepository<Tour> tourRepository;
+        private readonly IRepository<Tourguide> tourguideRepository;
+        private readonly IRepository<Language> languageRepository;
+        private readonly IRepository<OnHoliday> onHolidayRepository;
+        private readonly IRepository<Program> programRepository;
+        private readonly IRepository<Place> placeRepository;
+        private readonly IRepository<PLTCON> pltconRepository;
+        private readonly IRepository<PRTCON> prtconRepository;
+
+        public ReportBL(
+            IRepository<Report> reportRepository,
+            IRepository<Order> orderRepository,
+            IRepository<Customer> customerRepository,
+            IRepository<Tour> tourRepository,
+            IRepository<Tourguide> tourguideRepository,
+            IRepository<Language> languageRepository,
+            IRepository<OnHoliday> onHolidayRepository,
+            IRepository<Program> programRepository,
+            IRepository<Place> placeRepository,
+            IRepository<PLTCON> pltconRepository,
+            IRepository<PRTCON> prtconRepository)
+        {
+            this.reportRepository = reportRepository;
+            this.orderRepository = orderRepository;
+            this.customerRepository = customerRepository;
+            this.tourguideRepository = tourguideRepository;
+            this.tourRepository = tourRepository;
+            this.languageRepository = languageRepository;
+            this.onHolidayRepository = onHolidayRepository;
+            this.programRepository = programRepository;
+            this.placeRepository = placeRepository;
+            this.pltconRepository = pltconRepository;
+            this.prtconRepository = prtconRepository;
+        }
 
         /// <inheritdoc />
         public event EventHandler ReportListChanged;
