37ec8684eb9fe6584fd4db59130537dd6fe87d84
Barbara Böszöri <eboszori@gmail.com>
2018-04-29 19:52:36 +0200

    CustomerMainViewModel modified

--- HappyTourManager/HappyTourManager/ViewModels/CustomerMainViewModel.cs
+++ HappyTourManager/HappyTourManager/ViewModels/CustomerMainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BL;
+using DATA;
+using DATA.Repositoriees;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,101 @@ using System.Threading.Tasks;
 
 namespace HappyTourManager
 {
-    class CustomerMainViewModel
+    class CustomerMainViewModel : Bindable
     {
+        private CustomerRepository custRepository;
+        private CustomerBL custBL;
+
+        private List<string> searchCategories;
+
+        public List<string> SearchCategories
+        {
+            get
+            {
+                return searchCategories;
+            }
+
+            set
+            {
+                searchCategories = value;
+                OnPropertyChanged(nameof(SearchCategories));
+            }
+        }
+
+        public string SelectedCtegory
+        {
+            get
+            {
+                return selectedCtegory;
+            }
+
+            set
+            {
+                selectedCtegory = value;
+                OnPropertyChanged(nameof(SelectedCtegory));
+            }
+        }
+
+        public DateTime SelectedDateFrom
+        {
+            get
+            {
+                return selectedDateFrom;
+            }
+
+            set
+            {
+                selectedDateFrom = value;
+                OnPropertyChanged(nameof(SelectedDateFrom));
+            }
+        }
+
+        public DateTime SelectedDateTo
+        {
+            get
+            {
+                return selectedDateTo;
+            }
+
+            set
+            {
+                selectedDateTo = value;
+                OnPropertyChanged(nameof(SelectedDateTo));
+            }
+        }
+
+        public string SelectedValue
+        {
+            get
+            {
+                return selectedValue;
+            }
+
+            set
+            {
+                selectedValue = value;
+                OnPropertyChanged(nameof(SelectedValue));
+            }
+        }
+
+        private string selectedCtegory = "DEFAULT";
+        private string selectedValue;
+        private DateTime selectedDateFrom = DateTime.Today;
+        private DateTime selectedDateTo = DateTime.Today;
+
+        public CustomerMainViewModel(HappyTourDatabaseEntities entities)
+        {
+            custRepository = new CustomerRepository(entities);
+            custBL = new CustomerBL(custRepository);
+
+            searchCategories = new List<string>();
+            foreach (CustomerTerms item in Enum.GetValues(typeof(CustomerTerms)))
+            {
+                searchCategories.Add(item.ToString());
+            }
+        }
+
+
+
     }
 }
