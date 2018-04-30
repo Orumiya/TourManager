7e17d34d478653864f19b49f44db0f5b6d8d8d5b
Barbara Böszöri <eboszori@gmail.com>
2018-04-30 07:54:54 +0200

    MainVM constructor modified

--- HappyTourManager/HappyTourManager/ViewModels/MainViewModel.cs
+++ HappyTourManager/HappyTourManager/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BL;
 using DATA;
 using DATA.Repositoriees;
+using DATA.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +14,47 @@ namespace HappyTourManager
     {
         //private CustomerRepository custRepository;
         //private CustomerBL custBL;
-        HappyTourDatabaseEntities entities;
+        CustomerRepository customerRepo;
+        LanguageRepository languageRepo;
+        OfficeRepository officeRepo;
+        OnholidayRepository onHolidayRepo;
+        OrderRepository orderRepo;
+        PlaceRepository placeRepo;
+        PLTCONRepository pltconRepo;
+        ProgramRepository programRepo;
+        PRTCONRepository prtconRepo;
+        ReportRepository reportRepo;
+        TourguideRepository tourguideRepo;
+        TourRepository tourRepo;
+        UserRepository userRepo;
         private string selectedPage;
 
-        public MainViewModel(HappyTourDatabaseEntities entities)
+        public MainViewModel(CustomerRepository customerRepo,
+                            LanguageRepository languageRepo,
+                            OfficeRepository officeRepo,
+                            OnholidayRepository onHolidayRepo,
+                            OrderRepository orderRepo,
+                            PlaceRepository placeRepo,
+                            PLTCONRepository pltconRepo,
+                            ProgramRepository programRepo,
+                            PRTCONRepository prtconRepo,
+                            ReportRepository reportRepo,
+                            TourguideRepository tourguideRepo,
+                            TourRepository tourRepo
+                                )
         {
-            this.entities = entities;
+            this.customerRepo = customerRepo;
+            this.languageRepo = languageRepo;
+            this.officeRepo = officeRepo;
+            this.onHolidayRepo = onHolidayRepo;
+            this.orderRepo = orderRepo;
+            this.placeRepo = placeRepo;
+            this.pltconRepo = pltconRepo;
+            this.programRepo = programRepo;
+            this.prtconRepo = prtconRepo;
+            this.reportRepo = reportRepo;
+            this.tourguideRepo = tourguideRepo;
+            this.tourRepo = tourRepo;
             
         }
 
