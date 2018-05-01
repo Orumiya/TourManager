c2228a0dd5da8f660230b93db9a4726e9f0d12af
Barbara Böszöri <eboszori@gmail.com>
2018-05-01 18:53:33 +0200

    TourMainVM modified

--- HappyTourManager/HappyTourManager/ViewModels/TourMainViewModel.cs
+++ HappyTourManager/HappyTourManager/ViewModels/TourMainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DATA;
+using DATA.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,32 @@ using System.Threading.Tasks;
 
 namespace HappyTourManager
 {
-    class TourMainViewModel
+    class TourMainViewModel : Bindable
     {
+        private IRepository<Place> placeRepo;
+        private IRepository<PLTCON> pltconRepo;
+        private IRepository<Program> programRepo;
+        private IRepository<PRTCON> prtconRepo;
+        private IRepository<Tourguide> tourguideRepo;
+        private IRepository<Tour> tourRepo;
+
+
+
+        public TourMainViewModel(IRepository<Tour> tourRepo,
+            IRepository<Place> placeRepo,
+            IRepository<PLTCON> pltconRepo,
+            IRepository<Program> programRepo,
+            IRepository<PRTCON> prtconRepo,
+            IRepository<Tourguide> tourguideRepo)
+        {
+            this.tourRepo = tourRepo;
+            this.placeRepo = placeRepo;
+            this.pltconRepo = pltconRepo;
+            this.programRepo = programRepo;
+            this.prtconRepo = prtconRepo;
+            this.tourguideRepo = tourguideRepo;
+        }
+
+
     }
 }
