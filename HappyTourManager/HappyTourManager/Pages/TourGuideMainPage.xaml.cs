9df43f37309ea8ca7af70ad31e861511070ed2ae
Barbara Böszöri <eboszori@gmail.com>
2018-05-03 04:53:01 +0200

    TG page Loaded modified

--- HappyTourManager/HappyTourManager/Pages/TourGuideMainPage.xaml.cs
+++ HappyTourManager/HappyTourManager/Pages/TourGuideMainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DATA;
+using DATA.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,48 @@ namespace HappyTourManager.Pages
     /// </summary>
     public partial class TourGuideMainPage : Page
     {
-        public TourGuideMainPage()
+        #region private variables
+        private IRepository<Tourguide> tourGuideRepo;
+        private IRepository<Language> languageRepo;
+        private IRepository<OnHoliday> holidayRepo;
+        private IRepository<Tour> tourRepo;
+        private TourGuideMainViewModel tgVM;
+        private TGDetailsUC tgDetails;
+        #endregion
+
+        #region constructor
+        public TourGuideMainPage(
+                IRepository<Tourguide> tourGuideRepo,
+                IRepository<Language> languageRepo,
+                IRepository<OnHoliday> holidayRepo,
+                IRepository<Tour> tourRepo
+            )
         {
+            this.tourGuideRepo = tourGuideRepo;
+            this.languageRepo = languageRepo;
+            this.holidayRepo = holidayRepo;
+            this.tourRepo = tourRepo;
             InitializeComponent();
+
+            
+            
+        }
+        #endregion
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            tgVM = new TourGuideMainViewModel();
+            this.DataContext = tgVM;
+            //this.searchCat.ItemsSource = tgVM.SearchCategories;
+            this.searchCat.Visibility = Visibility.Visible;
+            this.searchCat.SelectedItem = "DEFAULT";
+            tgDetails = new TGDetailsUC();
+            //foreach (string item in tgVM.countryList)
+            //{
+            //    tgDetails.cboxCountry.Items.Add(item);
+            //}
+            this.contTGDetails.Content = tgDetails;
+            this.contTGDetails.Visibility = Visibility.Hidden;
         }
     }
 }
