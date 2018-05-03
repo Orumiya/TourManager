e83f3efdfdd1550399373e832073470b1805a747
Barbara Böszöri <eboszori@gmail.com>
2018-05-03 04:55:09 +0200

    modifications

--- HappyTourManager/HappyTourManager/ViewModels/TourGuideMainViewModel.cs
+++ HappyTourManager/HappyTourManager/ViewModels/TourGuideMainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DATA;
+using DATA.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace HappyTourManager
 {
     class TourGuideMainViewModel
     {
+        #region private variables
+        private IRepository<Tourguide> tourGuideRepo;
+        private IRepository<Language> languageRepo;
+        private IRepository<OnHoliday> holidayRepo;
+        private IRepository<Tour> tourRepo;
+        #endregion
     }
 }
