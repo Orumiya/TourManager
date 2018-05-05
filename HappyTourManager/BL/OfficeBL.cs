db68f044bd1bc80ed23a28cf3821105b8fd0cd4a
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-05-05 13:21:33 +0200

    OfficeBL added

---
+++ HappyTourManager/BL/OfficeBL.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="OfficeBL.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BL
+{
+    using System;
+    using BL.Interfaces;
+    using DATA;
+
+    public class OfficeBL : IOffice
+    {
+        public event EventHandler OfficeDataChanged;
+
+        public void Delete(Office office)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Office office)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
