47139b8ada8e6851e08c10dc6f0a17921675de56
Kasza Eszter <kasza.eszter.katalin@gmail.com>
2018-05-05 19:22:44 +0200

    OfficeBL implemented

--- HappyTourManager/BL/OfficeBL.cs
+++ HappyTourManager/BL/OfficeBL.cs
@@ -7,24 +7,55 @@ namespace BL
     using System;
     using BL.Interfaces;
     using DATA;
+    using DATA.Interfaces;
 
     public class OfficeBL : IOffice
     {
+        private readonly IRepository<Office> officeRepository;
+
+        public OfficeBL(IRepository<Office> officeRepository)
+        {
+            this.officeRepository = officeRepository;
+        }
+
+        /// <inheritdoc />
         public event EventHandler OfficeDataChanged;
 
+        /// <inheritdoc />
         public void Delete(Office office)
         {
-            throw new NotImplementedException();
+            try
+            {
+                this.officeRepository.Delete(office);
+            }
+            finally
+            {
+                this.OnOfficeChanged();
+            }
         }
 
+        /// <inheritdoc />
         public void Save(Office office)
         {
-            throw new NotImplementedException();
+            try
+            {
+                this.officeRepository.Create(office);
+            }
+            finally
+            {
+                this.OnOfficeChanged();
+            }
         }
 
-        public void Update()
+        /// <inheritdoc />
+        public void Update() => this.officeRepository.Update();
+
+        /// <summary>
+        /// notifies the outside about any collection change manually
+        /// </summary>
+        private void OnOfficeChanged()
         {
-            throw new NotImplementedException();
+            this.OfficeDataChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 }
